7567b9c3966434aab0964e301dfb2f5272e6696a
Gavin97Dmello <gavin@zaya.in>
2020-09-09 11:10:37 +0530

    Initial Commit

---
+++ Learn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learn
+//
+//  Created by Zaya Developer on 09/09/20.
+//  Copyright © 2020 Zaya Developer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
